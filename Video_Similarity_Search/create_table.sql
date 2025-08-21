f29d1c28237b383b55da8399d5a83a21be484b23
phithuan <turnthuan1@gmail.com>
2025-08-21 21:09:44 +0700

    cmt

--- Video_Similarity_Search/create_table.sql
+++ Video_Similarity_Search/create_table.sql
@@ -1,18 +1,21 @@
--- Tạo bảng cho video
-    CREATE TABLE IF NOT EXISTS videos (
-        id SERIAL PRIMARY KEY,
-        video_path VARCHAR(255) UNIQUE,
-        title VARCHAR(255),
-        description TEXT
+-- create_table.sql
+CREATE TABLE IF NOT EXISTS videos (
+    id SERIAL PRIMARY KEY,
+    video_path VARCHAR(255) UNIQUE,
+    title VARCHAR(255),
+    description TEXT
 );
 
--- Tạo bảng cho ánh xạ khung hình-video
-    CREATE TABLE IF NOT EXISTS frame_mappings (
-        frame_id SERIAL PRIMARY KEY,
-        video_id INTEGER REFERENCES videos(id),
-        frame_path VARCHAR(255),
-        milvus_id BIGINT
+CREATE TABLE IF NOT EXISTS frame_mappings (
+    frame_id SERIAL PRIMARY KEY,
+    video_id INTEGER REFERENCES videos(id),
+    frame_path VARCHAR(255) UNIQUE,
+    pts_time FLOAT,
+    frame_idx INT,
+    fps INT,
+    milvus_id BIGINT
 );
+
 
 ALTER TABLE videos
 ADD CONSTRAINT unique_video_path UNIQUE (video_path);
