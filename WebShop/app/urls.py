5c24c1e26c87d55b6a5c5cf450c9184ccde1558e
phithuan <turnthuan1@gmail.com>
2025-09-05 22:25:32 +0700

    giao dien web django

--- WebShop/app/urls.py
+++ WebShop/app/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),  # đặt luôn name để dễ gọi {% url 'home' %}
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+
+    # Thêm route chi tiết sản phẩm
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+
+    # Thêm route thêm vào giỏ hàng
+    path('add-to-cart/<int:pk>/', views.add_to_cart, name='add_to_cart'),
 ]
